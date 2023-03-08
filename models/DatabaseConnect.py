3650b95e3bfabdb3bf42e90318f0eca00b5d53bd
Stephen Leveckis <LEVECKIS@research.local>
2023-03-07 16:18:41 -0800

    everything sort of works and is also sort of broken. checkpoint for now

--- models/DatabaseConnect.py
+++ models/DatabaseConnect.py
@@ -1,5 +1,6 @@
 import psycopg2
 import pandas as pd
+from urllib.parse import urlparse
 
 class DatabaseConnector():
     """
@@ -24,8 +25,9 @@ class DatabaseConnector():
         
     """
     def __init__(self):
+        self.conn_string ='postgresql://postgres:l1v1ngD4t4b4s3!@10.67.10.38:5005'
         self.cursor = self.cursor_to_server()
-        self.databases = self.get_databases()
+        self.master_dict = self.get_databases_and_tables()
 
     def cursor_to_server(self):
         """
@@ -34,53 +36,110 @@ class DatabaseConnector():
             cur
                 A connection to the living database
         """
-        conn_string = 'postgresql://postgres:l1v1ngD4t4b4s3!@10.67.10.38:5005'
-        pg_conn = psycopg2.connect(conn_string)
+        conn_string_old = 'postgresql://postgres:l1v1ngD4t4b4s3!@10.67.10.38:5005'
+        # Parsing old string into a dictionary so I can change dbname easily
+        p = urlparse(conn_string_old)
+        pg_connection_dict = {
+            'dbname': p.path[1:],
+            'user': p.username,
+            'password': p.password,
+            'port': p.port,
+            'host': p.hostname
+        }
+        pg_conn = psycopg2.connect(**pg_connection_dict)
         cur = pg_conn.cursor()
         return cur
 
-    def get_databases(self):
+    def get_databases_and_tables(self):
         """
         Returns:
         ---------
             database_dict
                 A dictionary of all the databases within the living database
         """
-        # Replace with query from Mike to get a dictionary as in the docstring
-
-        self.cursor.execute("""SELECT datname FROM pg_catalog.pg_database""")
+        # First, get all databases into a list, database_list
+        fetch_query = ("""SELECT datname FROM pg_catalog.pg_database""")
+        self.cursor.execute(fetch_query)
         databases = self.cursor.fetchall()
         database_list = []
         for d in databases:
             database_list.append(d[0])
+        num_databases = len(database_list)
 
-        test_dba = {
-            "CoolDatabase":  ["tableForTwo", "cooltableNice", "mySweetTable", "usefulTable", "niceTableToHave"],
-            "EvilDatabase":  ["villainousTable"],
-            "RegularDatabase":  ["tableOfHotDogs", "roundTable", "partyTable"],
-            "ScienceDatabase":  ["tableOfFrogs", "taleOfNewts", "coolRocks.bizTable"],
-            "UselessDatabase":  ["oldTable", "emptyTable", "corruptTable"],
-            "ThisIsADatabase":  ["thatTable", "thisTable", "standardTable", "extraTable"]
+        for d in database_list:
+            print(d)
+        print("Number of databases: ", num_databases)
+
+        # Then, connect to each database and store its tables 
+        p = urlparse(self.conn_string)
+        temp_connection_dict = {
+            'dbname': d,
+            'user': p.username,
+            'password': p.password,
+            'port': p.port,
+            'host': p.hostname
         }
-        return test_dba
-        #return (database_list)
+
+        master_dictionary = {}
+
+        print("Gathering database info... this may take a second...")
+        # Connect to each database and save all its table names to a list
+        for d in database_list:
+            temp_connection_dict['dbname'] = d
+            try:
+                temp_conn = psycopg2.connect(**temp_connection_dict)
+            except:
+                print("\tRats! Failed on: ", temp_connection_dict['dbname'])
+                # If you don't remove failed databases, master dictionary keys/values will be mismatched later
+                database_list.remove(d)
+            else:
+
+                cur = temp_conn.cursor()
+                # This query gets a list of all table names from the current database (d in database_list)
+                cur.execute("""SELECT table_name FROM information_schema.tables WHERE table_schema = 'postgres' and table_type = 'BASE TABLE' or table_schema = 'public' and table_type = 'BASE TABLE';""")
+                temp_list = []
+                for t in cur.fetchall():
+                    temp_list.append(t[0])
+
+                # Create a dictionary entry of the corresponding database and list of tables
+                master_dictionary[d] = temp_list
+
+                # Uncomment these to get terminal printout of database name and its tables
+                """
+                print("Success on: ", temp_connection_dict['dbname'])
+                print(temp_list)
+                print("----------------------")
+                """
+        return master_dictionary
 
     #TODO: Because we're getting database names and table names, do a try block or something. Or use a filter. 
     # Don't want to query a tablename where tablename is actually a databasename
-    def get_table_contents(self, table=None, n=None):
+
+    def get_table_contents(self, database=None, table=None, n=None):
+        # Placeholder code... switch out with a real query with table name
         test_table = {
             "What Rock" : ["Basalt", "shale", "obsidian", "quartz", "sdfsfs", "asdfasdf", "sdfasdf", "asdf", "marble"],
             "How old is rock" : [101, 202, 333, 40151, 1551515, 663, 991, 123, 12],
             "calories" : [123, 4141, 52343, 123123, 235234, 5345, 2312, 534534, 12313],
             "phone number" : [23123, 12312, 53453, 867, 56567, 9878990 , 56986, 234243, 90897]
         }
-
-        # Placeholder code... switch out with a real query with table name
-        test_table["What Rock"][0] = table
-        df = pd.DataFrame(test_table)
-
+        
+        """
         # SQL Query where we get the first n rows of table 'table'
         # return that dataframe instead
-        return df
+        query = '''SELECT * FROM ''' + table + ''' LIMIT 10'''
+        print ("Q: ", type(query), query)
+        try:
+            self.cursor.execute(query)
+        except:
+            print("table fetch execute Didn't work!")
+       """ 
+        conn_string = self.conn_string + '/' + database
 
+        with psycopg2.connect(conn_string) as pg_conn:
+            sql = "select * from {0} LIMIT 100".format(table)
+            df2 = pd.read_sql_query(sql, pg_conn)
 
+        test_table["What Rock"][0] = table
+        df = pd.DataFrame(test_table)
+        return df2
--- uimain.py
+++ uimain.py
@@ -49,7 +49,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Column Model/View
         self.db_connection = DatabaseConnector()
-        self.col_model = CustomQColumnModel(self.db_connection.databases)
+        self.col_model = CustomQColumnModel(self.db_connection.master_dict)
         self.col_model.generate_tree() 
         self.ColumnView.setModel(self.col_model)
         # only disables innermost list. similar code in models.py for outer list
@@ -69,14 +69,45 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.ColumnView.clicked.connect(self.colViewClick)
 
     # Slots
+    """
+        Get a table as a dataframe and set it as the TableView's model so the user can see it
+    """
     def colViewClick(self):
-        index = self.ColumnView.selectedIndexes()[0]
-        item = self.col_model.itemFromIndex(index).text()
+        # Get the text of whatever table the user has selected
+        user_select_index = self.ColumnView.selectedIndexes()[0]
+        item = user_select_index.data()
+        print("userselectindex:", item)
+        # item = self.col_model.itemFromIndex(user_select_index).text()
         print("Item: ", item)
-        table = self.db_connection.get_table_contents(item, self.tableSize)
-        print(table)
-        self.table_model = CustomQTableModel(table)
-        self.TableView.setModel(self.table_model)
+
+        # Find relevant database name to chosen table (we need this to query the living database)
+        dict_keys = list(self.db_connection.master_dict.keys())
+        dict_vals = list(self.db_connection.master_dict.values())
+        # Note that we don't actually care about where the item is in 
+        # the list, we just need the list's index in the meta-list
+        dict_index = None
+        print("Dict index (should be None):", dict_index)
+        for count, table in enumerate(dict_vals):
+            try:
+                table.index(item)
+            except:
+                pass
+            else:
+                dict_index = count
+
+        
+        if dict_index is not None:
+            db_name = dict_keys[dict_index]
+            print("Dict index:", dict_index)
+            print("Database: ", db_name)
+            print("Table: ", item)
+            print("--------------------------------")
+
+            # Get table as a pandas dataframe, passing in database name and table name
+            table = self.db_connection.get_table_contents(database=db_name, table=item, n=self.tableSize)
+            # Make a table model with the dataframe, and set the View to it
+            self.table_model = CustomQTableModel(table)
+            self.TableView.setModel(self.table_model)
 
 
 app = QApplication(sys.argv)
